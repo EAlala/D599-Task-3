071536e56f307db11e136274a5aac26755fb9378
Edgar <yeai22@live.com>
2025-10-18 13:39:03 -0500

    copy of data set

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import pandas as pd
-from sklearn.preprocessing import OrdinalEncoder, LabelEncoderimport 
+from sklearn.preprocessing import OrdinalEncoder, LabelEncoder
 import numpy as np
 
+#Load data set
 data_set = pd.read_csv("C:/Users/yeai2_6rsknlh/OneDrive/Visual/D599 Task 3/Megastore Dataset.csv")
+
+#Create copy of data set
+data_set_encoded = data_set.copy()
+
+
