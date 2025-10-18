3e8e39fd1d70db032ac3245fd4ef6c54c19a65f6
Edgar <yeai22@live.com>
2025-10-18 13:32:50 -0500

    imports & import data set

---
+++ main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from sklearn.preprocessing import OrdinalEncoder, LabelEncoderimport 
+import numpy as np
+
+data_set = pd.read_csv("C:/Users/yeai2_6rsknlh/OneDrive/Visual/D599 Task 3/Megastore Dataset.csv")
