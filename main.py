fdc2030374877fb4656b3c536fb6c7b76f33759b
Edgar <yeai22@live.com>
2025-10-18 14:13:14 -0500

    adjsutment

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ data_set_encoded = data_set.copy()
 priority_map = {"critical" : 3, "High" : 2, "Medium" : 1, "Low" : 0}
 data_set_encoded["OrderPriority_encoded"] =  data_set_encoded["OrderPriority"].map(priority_map)
 
-#Customer satisdaction encoding
+#Customer satisfaction encoding
 satisfaction_map = {"Very Dissatisfied": 0, "Dissatisfied": 1, "Neutral": 2, "Satisfied": 3,"Very Satisfied": 4}
-data_set_encoded = ["satisfaction_encoded"] = data_set_encoded["CustomerOrderSatisfaction"].map("satisfaction_map")
+data_set_encoded["Satisfaction_encoded"] = data_set_encoded["CustomerOrderSatisfaction"].map("satisfaction_map")
+
+#Region encoding
